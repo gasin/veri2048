a8322a4f7183d6087565c5a3432f830c2a29d92c
gasin <gasinsato@gmail.com>
2017-08-02 10:11:24 +0900

    implement board

--- display.v
+++ display.v
@@ -1,5 +1,5 @@
-`define CELL_SIZE 100
-`define BORDER_WIDTH 15
+//`define CELL_SIZE 100
+//`define BORDER_WIDTH 15
 
 module display(row, col, red, green, blue, color, up, down, left, right, vnotactive, CLK, RST);
 	input [31:0] row, col;
@@ -8,6 +8,9 @@ module display(row, col, red, green, blue, color, up, down, left, right, vnotact
 	reg red, green, blue;
 	//reg [31:0] originX, originY;
 	
+	parameter CELL_SIZE = 100;
+	parameter BORDER_WIDTH = 10;
+	
 	always @(posedge CLK or negedge RST) begin
 		if(!RST) begin
 			red <= 1'b1;
@@ -15,55 +18,62 @@ module display(row, col, red, green, blue, color, up, down, left, right, vnotact
 			blue <= 1'b1;
 		end
 		else begin
-			if(row >= CELL_SIZE-BORDER_WIDTH && row <= CELL_SIZE+BORDER_WIDTH) begin
-				red <= 1'b0;
-				green <= 1'b0;
-				blue <= 1'b0;
-			end
-			else if(row >= CELL_SIZE*2-BORDER_WIDTH && row <= CELL_SIZE*2+BORDER_WIDTH) begin
-				red <= 1'b0;
-				green <= 1'b0;
-				blue <= 1'b0;
-			end
-			else if(row >= CELL_SIZE*3-BORDER_WIDTH && row <= CELL_SIZE*2+BORDER_SIZE) begin
-				red <= 1'b0;
-				green <= 1'b0;
-				blue <= 1'b0;
-			end
-			else if(row >= CELL_SIZE*4-BORDER_WIDTH && row <= CELL_SIZE*4+BORDER_SIZE) begin
-				red <= 1'b0;
-				green <= 1'b0;
-				blue <= 1'b0;
-			end
-			else if(row >= CELL_SIZE*5-BORDER_WIDTH && row <= CELL_SIZE*5+BORDER_SIZE) begin
-				red <= 1'b0;
-				green <= 1'b0;
-				blue <= 1'b0;
-			end
-			else if(col >= CELL_SIZE-BORDER_WIDTH && col <= CELL_SIZE+BORDER_WIDTH) begin
-				red <= 1'b0;
-				green <= 1'b0;
-				blue <= 1'b0;
-			end
-			else if(col >= CELL_SIZE*2-BORDER_WIDTH && col <= CELL_SIZE*2+BORDER_WIDTH) begin
-				red <= 1'b0;
-				green <= 1'b0;
-				blue <= 1'b0;
-			end
-			else if(col >= CELL_SIZE*3-BORDER_WIDTH && col <= CELL_SIZE*2+BORDER_SIZE) begin
-				red <= 1'b0;
-				green <= 1'b0;
-				blue <= 1'b0;
-			end
-			else if(col >= CELL_SIZE*4-BORDER_WIDTH && col <= CELL_SIZE*4+BORDER_SIZE) begin
-				red <= 1'b0;
-				green <= 1'b0;
-				blue <= 1'b0;
-			end
-			else if(col >= CELL_SIZE*5-BORDER_WIDTH && col <= CELL_SIZE*5+BORDER_SIZE) begin
-				red <= 1'b0;
-				green <= 1'b0;
-				blue <= 1'b0;
+			if(col >= CELL_SIZE*2-BORDER_WIDTH && col <= CELL_SIZE*6+BORDER_WIDTH && row >= CELL_SIZE-BORDER_WIDTH && row <= CELL_SIZE*5+BORDER_WIDTH) begin
+				if(row >= CELL_SIZE-BORDER_WIDTH && row <= CELL_SIZE+BORDER_WIDTH) begin
+					red <= 1'b0;
+					green <= 1'b0;
+					blue <= 1'b0;
+				end
+				else if(row >= CELL_SIZE*2-BORDER_WIDTH && row <= CELL_SIZE*2+BORDER_WIDTH) begin
+					red <= 1'b0;
+					green <= 1'b0;
+					blue <= 1'b0;
+				end
+				else if(row >= CELL_SIZE*3-BORDER_WIDTH && row <= CELL_SIZE*3+BORDER_WIDTH) begin
+					red <= 1'b0;
+					green <= 1'b0;
+					blue <= 1'b0;
+				end
+				else if(row >= CELL_SIZE*4-BORDER_WIDTH && row <= CELL_SIZE*4+BORDER_WIDTH) begin
+					red <= 1'b0;
+					green <= 1'b0;
+					blue <= 1'b0;
+				end
+				else if(row >= CELL_SIZE*5-BORDER_WIDTH && row <= CELL_SIZE*5+BORDER_WIDTH) begin
+					red <= 1'b0;
+					green <= 1'b0;
+					blue <= 1'b0;
+				end
+				else if(col >= CELL_SIZE*2-BORDER_WIDTH && col <= CELL_SIZE*2+BORDER_WIDTH) begin
+					red <= 1'b0;
+					green <= 1'b0;
+					blue <= 1'b0;
+				end
+				else if(col >= CELL_SIZE*3-BORDER_WIDTH && col <= CELL_SIZE*3+BORDER_WIDTH) begin
+					red <= 1'b0;
+					green <= 1'b0;
+					blue <= 1'b0;
+				end
+				else if(col >= CELL_SIZE*4-BORDER_WIDTH && col <= CELL_SIZE*4+BORDER_WIDTH) begin
+					red <= 1'b0;
+					green <= 1'b0;
+					blue <= 1'b0;
+				end
+				else if(col >= CELL_SIZE*5-BORDER_WIDTH && col <= CELL_SIZE*5+BORDER_WIDTH) begin
+					red <= 1'b0;
+					green <= 1'b0;
+					blue <= 1'b0;
+				end
+				else if(col >= CELL_SIZE*6-BORDER_WIDTH && col <= CELL_SIZE*6+BORDER_WIDTH) begin
+					red <= 1'b0;
+					green <= 1'b0;
+					blue <= 1'b0;
+				end
+				else begin
+					red <= 1'b1;
+					green <= 1'b1;
+					blue <= 1'b1;
+				end
 			end
 			else begin
 				red <= 1'b1;
