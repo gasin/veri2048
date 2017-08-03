cb64b2192426d05ba4c527f4e53eb35cd1d22882
gasin <gasinsato@gmail.com>
2017-08-03 14:10:32 +0900

    fix bug

--- src/convert.py
+++ src/convert.py
@@ -20,7 +20,7 @@ size = bi_im.size
 for x in range(size[0]):
     for y in range(size[1]):
         if bi_im.getpixel((x, y)) == 0:
-            print "else if(row >= cell_position_row[cell_index] + 20 +"+ str(y) + "*" + str(thick) + " && row < cell_position_row[cell_index]+20+(" + str(y) + "+1)*" + str(thick) + " && col >= cell_position_col[cell_index]+4+" + str(left*32) + "+" + str(x) + "*" + str(thick) + " && col < cell_position_col[cell_index]+4+" + str(left*32) + "+(" + str(x) + "+1)*" + str(thick) + ") begin"
+            print "else if(row >= cell_position_row[cell_index] + 25 +"+ str(y) + "*" + str(thick) + " && row < cell_position_row[cell_index]+25+(" + str(y) + "+1)*" + str(thick) + " && col >= cell_position_col[cell_index]+8+" + str(left*32) + "+" + str(x) + "*" + str(thick) + " && col < cell_position_col[cell_index]+8+" + str(left*32) + "+(" + str(x) + "+1)*" + str(thick) + ") begin"
             print "\t{red, green, blue} <= 3'b000;"
             print "end"
             
