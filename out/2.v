8cec0023b291ff7a3006dff0494f43d0e3eeeb8c
gasin <gasinsato@gmail.com>
2017-08-03 11:41:25 +0900

    add 2.v again

---
+++ out/2.v
@@ -0,0 +1,54 @@
+else if(row >= cell_position_row[cell_index] + 20 +3*thick && row < cell_position_row[cell_index]+20+(3+1)*thick && col >= cell_position_col[cell_index]+20+2*thick && col < cell_position_col[cell_index]+20+(2+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +7*thick && row < cell_position_row[cell_index]+20+(7+1)*thick && col >= cell_position_col[cell_index]+20+2*thick && col < cell_position_col[cell_index]+20+(2+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +1*thick && row < cell_position_row[cell_index]+20+(1+1)*thick && col >= cell_position_col[cell_index]+20+3*thick && col < cell_position_col[cell_index]+20+(3+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +2*thick && row < cell_position_row[cell_index]+20+(2+1)*thick && col >= cell_position_col[cell_index]+20+3*thick && col < cell_position_col[cell_index]+20+(3+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +6*thick && row < cell_position_row[cell_index]+20+(6+1)*thick && col >= cell_position_col[cell_index]+20+3*thick && col < cell_position_col[cell_index]+20+(3+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +7*thick && row < cell_position_row[cell_index]+20+(7+1)*thick && col >= cell_position_col[cell_index]+20+3*thick && col < cell_position_col[cell_index]+20+(3+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +1*thick && row < cell_position_row[cell_index]+20+(1+1)*thick && col >= cell_position_col[cell_index]+20+4*thick && col < cell_position_col[cell_index]+20+(4+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +5*thick && row < cell_position_row[cell_index]+20+(5+1)*thick && col >= cell_position_col[cell_index]+20+4*thick && col < cell_position_col[cell_index]+20+(4+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +7*thick && row < cell_position_row[cell_index]+20+(7+1)*thick && col >= cell_position_col[cell_index]+20+4*thick && col < cell_position_col[cell_index]+20+(4+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +1*thick && row < cell_position_row[cell_index]+20+(1+1)*thick && col >= cell_position_col[cell_index]+20+5*thick && col < cell_position_col[cell_index]+20+(5+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +2*thick && row < cell_position_row[cell_index]+20+(2+1)*thick && col >= cell_position_col[cell_index]+20+5*thick && col < cell_position_col[cell_index]+20+(5+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +5*thick && row < cell_position_row[cell_index]+20+(5+1)*thick && col >= cell_position_col[cell_index]+20+5*thick && col < cell_position_col[cell_index]+20+(5+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +7*thick && row < cell_position_row[cell_index]+20+(7+1)*thick && col >= cell_position_col[cell_index]+20+5*thick && col < cell_position_col[cell_index]+20+(5+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +3*thick && row < cell_position_row[cell_index]+20+(3+1)*thick && col >= cell_position_col[cell_index]+20+6*thick && col < cell_position_col[cell_index]+20+(6+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +4*thick && row < cell_position_row[cell_index]+20+(4+1)*thick && col >= cell_position_col[cell_index]+20+6*thick && col < cell_position_col[cell_index]+20+(6+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +7*thick && row < cell_position_row[cell_index]+20+(7+1)*thick && col >= cell_position_col[cell_index]+20+6*thick && col < cell_position_col[cell_index]+20+(6+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +7*thick && row < cell_position_row[cell_index]+20+(7+1)*thick && col >= cell_position_col[cell_index]+20+7*thick && col < cell_position_col[cell_index]+20+(7+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
+else if(row >= cell_position_row[cell_index] + 20 +7*thick && row < cell_position_row[cell_index]+20+(7+1)*thick && col >= cell_position_col[cell_index]+20+8*thick && col < cell_position_col[cell_index]+20+(8+1)*thick)) begin
+	{red, green, blue} <= 3'b000
+end
